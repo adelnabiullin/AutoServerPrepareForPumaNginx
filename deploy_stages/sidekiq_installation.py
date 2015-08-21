8cedeb8b232d96f1a1dc7de58612f4b89aeeea67
Ivan <vanyaz158@gmail.com>
2015-08-22 00:26:50 +0300

    Remove deploying sidekiq yml file and remove sidekiq gem installation

--- deploy_stages/sidekiq_installation.py
+++ deploy_stages/sidekiq_installation.py
@@ -1,17 +1,8 @@
 from fabric.api import local, lcd, settings, run, sudo, env, put, cd
 import settings as project_settings
 
-def sidekiq_installation():
-    run('gem install sidekiq')
-
 def upload_upstart_script():
-    upload_file_and_move_it('sidekiq.conf', '/etc/init/sidekiq.conf')
-
-def upload_yml_configuration():
-    run('mkdir -p ~/sidekiq_config')
-    upload_file_and_move_it('sidekiq_conf.yml', '~/sidekiq_config/sidekiq_conf.yml')
+    upload_file_and_move_it('sidekiq.conf', '/etc/init/sidekiq.conf'
 
 def prepare_sidekiq_for_deploy():
-    sidekiq_installation()
     upload_upstart_script()
-    upload_yml_configuration()
