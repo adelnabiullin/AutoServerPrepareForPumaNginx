8e38c2b6b5942ccba510c1e1c82257af37f4271b
Ivan <vanyaz158@gmail.com>
2015-09-03 15:28:18 +0300

    Sidekiq upstart script installation

--- deploy_stages/additional_lib_installation.py
+++ deploy_stages/additional_lib_installation.py
@@ -2,9 +2,13 @@ from fabric.api import local, lcd, settings, run, sudo, env, put, cd
 import settings as project_settings
 
 def install_rubinus_dependies():
-    sudo('sudo apt-get install -y bison \
+    sudo('apt-get install -y bison \
 zlib1g-dev libssl-dev libreadline-dev libncurses5-dev')
     sudo('apt-get install -y ruby-dev llvm')
 
+def install_imagemagick_dependies():
+    sudo('apt-get install -y imagemagick')
+
 def install_all_dependies():
     install_rubinus_dependies()
+    install_imagemagick_dependies()
--- deploy_stages/sidekiq_installation.py
+++ deploy_stages/sidekiq_installation.py
@@ -2,8 +2,16 @@ from fabric.api import local, lcd, settings, run, sudo, env, put, cd
 import settings as project_settings
 from helpers import upload_file_and_move_it
 
-def upload_upstart_script():
+def upload_sidekiq_manager_script():
+    upload_file_and_move_it('sidekiq-manager.conf', '/etc/init/sidekiq-manager.conf ')
+
+def upload_sidekiq_script():
     upload_file_and_move_it('sidekiq.conf', '/etc/init/sidekiq.conf')
 
+def create_sidekiq_projects_manager():
+    sudo('touch /etc/sidekiq.conf')
+
 def prepare_sidekiq_for_deploy():
-    upload_upstart_script()
+    upload_sidekiq_manager_script()
+    upload_sidekiq_script()
+    create_sidekiq_projects_manager()
